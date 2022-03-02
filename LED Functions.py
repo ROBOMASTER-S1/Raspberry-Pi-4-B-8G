e9b28122c679e2124e57e14f1a79802a038214c1
Joseph C. Richardson <josephcrichardson08@gmail.com>
2022-03-02 13:50:18 -0500

    Update LED Functions.py

--- LED Functions.py
+++ LED Functions.py
@@ -20,7 +20,66 @@
 
 # Raspberry Pi 4 Python program example:
 
-# Here are some function samples of blinking.
+# Here are some function samples of blinking
+# and breathing leds.
+
+# First, place ten red leds in a single
+# row. Next, place two yellow leds, one
+# on each end of the ten red leds. Next
+# leave some room for the RGB led, which
+# will take three 220 ohm resistors on
+# each positive node. Make sure the longer
+# node is either in 3v or ground. My RGB
+# led takes the 3v pin on the Ras pi. Now
+# place RGB led right smack in the middle
+# of the red leds.
+
+# Note: if your RGB takes 3v, then make sure
+# you don't place its three resistors on the
+# ground rails, where all the red leds resistors
+# are located. Instead, make sure you have
+# jumper wires connected to three GPIO pins
+# either in front of each of the three resistors
+# or behind them. I keep my jumper wires behind
+# all my resistors as much as I can. It looks
+# less confusing and neat at the same time.
+# However, in my case, my jumper wires are all
+# behind my leds, but not behind my resistors.
+# My RGB shows the example I like best. All
+# depending on your breadboard layout is what
+# matters the most. Try to layout your work
+# like you want it, but make sure you set up
+# everything to connect the way you want.
+# Yet, why not use one ground wire to feed all
+# your ten red leds, instead of having all kinds
+# of extra ground wires connected to each led.
+# Sometimes, you've just got to go for the
+# betterment of the two setups, I've tried so
+# far. That's why I cannot stick my jumper
+# wires right behind my resistors, like I like
+# them to be. But instead, I took the betterment
+# of the two and simply placed my jumpers
+# directly in front of all my resistors, except
+# for the RGB led, which has its own 3v supply.
+# All the other 12 leds including the yellow
+# leds, all have their resistors connected
+# to the ground rail on my breadboard setup.
+# In all, you get to play with 13 leds, when
+# you consider the RGB led as the 13th.
+
+# I hope other Raspberry Pi owners will like
+# this Python program to get them going into
+# the Raspberry Pi, no matter what kind; I
+# have a Ras Pi 4. I just got it, but I'm
+# Hooooked!!!!!!!!
+
+# Note: I will be doing a video about my
+# breadboard and jumper wire layout in the
+# near future. This video will show you
+# how to use this Raspberry Pi 4 Python
+# program example with the very same bread-
+# board layout, I use so you can follow
+# right along with my video example.
 
 import RPi.GPIO as GPIO
 from time import sleep as wait
@@ -35,6 +94,8 @@ yellow_leds=[7,37]
 RGB_led=[18,16,12]
 
 RGB_mix=[[18,12],[18,16],[16,12]]
+
+hz=500 # LED dimmer herz
 
 led_speed=.1
 
@@ -433,7 +494,43 @@ def red_leds_stack_outward():
     for i in red_leds:
         GPIO.output(i,0)
 
+def red_leds_breathe():
+    
+    a0=GPIO.PWM(red_leds[0],hz)
+    a1=GPIO.PWM(red_leds[1],hz)
+    a2=GPIO.PWM(red_leds[2],hz)
+    a3=GPIO.PWM(red_leds[3],hz)
+    a4=GPIO.PWM(red_leds[4],hz)
+    a5=GPIO.PWM(red_leds[5],hz)
+    a6=GPIO.PWM(red_leds[6],hz)
+    a7=GPIO.PWM(red_leds[7],hz)
+    a8=GPIO.PWM(red_leds[8],hz)
+    a9=GPIO.PWM(red_leds[9],hz)    
+
+    dimmer=(a0,a1,a2,a3,a4,a5,a6,a7,a8,a9)
+
+    for x in range(2):
+        GPIO.output(RGB_led[0],0)
+        for i in range(0,50,3):
+            for j in dimmer:
+                j.start(0)
+                j.ChangeDutyCycle(i)
+                wait(.01)
+
+        for i in range(50,-1,-3):
+            GPIO.output(RGB_mix[1],0)
+            for j in yellow_leds:
+                GPIO.output(j,1)
+            for j in dimmer:
+                j.start(0)
+                j.ChangeDutyCycle(i)
+                wait(.01)
+            GPIO.output(RGB_mix[1],1)
+            for i in yellow_leds:
+                GPIO.output(i,0)
+                
 led_functions=[
+    red_leds_breathe,
     red_led_single_right,
     red_led_single_left_inverse,
     red_leds_side_to_side_right,
@@ -462,4 +559,4 @@ while True:
         print('cleanup/release all GPIO pinouts \
 to LOW state.')
         GPIO.cleanup()
-        break    
+        break
