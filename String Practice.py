810cc51a2ba72c585ca48660c197efaad18ec3b4
Joseph C. Richardson <josephcrichardson08@gmail.com>
2022-05-04 13:26:32 -0400

    Create String Practice.py

---
+++ String Practice.py
@@ -0,0 +1,168 @@
+import RPi.GPIO as GPIO,drivers,threading
+from time import sleep as wait
+
+GPIO.setmode(GPIO.BOARD) # breadboard method
+GPIO.setwarnings(False) # disable setwarnings
+display=drivers.Lcd() # enable the LCD display
+
+display.lcd_clear() # clear the LCD screen
+
+latch=33
+data_bit=35
+clock=31
+
+msbs=65535,65536
+lsbs=32767,32768
+
+on_off=0,1,0,1,0,1,0
+
+RGB_led1=[13,11,7]
+RGB_led2=[21,19,15]
+
+RGB_mix1=[[13,11],[13,7],[11,7]]
+RGB_mix2=[[21,19],[21,15],[19,15]]
+
+blue_green='''
+GPIO.output(RGB_led1[2],1)
+GPIO.output(RGB_led2[1],1)'''
+
+green_blue='''
+GPIO.output(RGB_led1[1],1)
+GPIO.output(RGB_led2[2],1)'''
+
+red_yellow='''
+GPIO.output(RGB_led1[0],1)
+GPIO.output(RGB_mix2[0],1)'''
+
+yellow_red='''
+GPIO.output(RGB_led2[0],1)
+GPIO.output(RGB_mix1[0],1)'''
+
+pink_cyan='''
+GPIO.output(RGB_mix1[1],1)
+GPIO.output(RGB_mix2[2],1)'''
+
+cyan_pink='''
+GPIO.output(RGB_mix1[2],1)
+GPIO.output(RGB_mix2[1],1)'''
+
+RGB_off='''
+for i in RGB_led1,RGB_led2:
+    GPIO.output(i,0)'''
+
+stop_program_message='''
+print('Stop program Execution/run:')
+print('cleanup/release all GPIO pinouts \
+to LOW state.')'''
+
+title='16b BINARY COUNT ','Python Program  '
+programmer='BY Joseph C. Richardson, GitHub.com '
+
+led_speed=.5
+
+for i in RGB_led1,RGB_led2:
+    GPIO.setup(i,GPIO.OUT)
+    GPIO.output(i,0)
+
+led_loop1=blue_green,yellow_red
+led_loop2=green_blue,pink_cyan
+led_loop3=red_yellow,cyan_pink
+
+loop_tuple=led_loop1,led_loop2,led_loop3
+
+def RGB_loop():
+    
+    while True:
+        for i in range(2):
+            for j in loop_tuple:
+                exec(j[i]);wait(.2)
+                exec(RGB_off)
+
+control_shift=latch,data_bit,clock
+
+for i in control_shift:GPIO.setup(i,GPIO.OUT)
+    
+for i in range(16):            
+    GPIO.output(latch,0)
+    GPIO.output(data_bit,0)
+    GPIO.output(clock,1)
+    GPIO.output(latch,1)
+    GPIO.output(clock,0)
+
+while True:
+    display.lcd_display_string(title[0],1)
+    display.lcd_display_string(title[1],2)
+    wait(2)
+    try:
+        display.lcd_display_string(title[0],1)
+        display.lcd_display_string(programmer,2)
+        wait(1)
+        
+        for i in range(len(programmer)):
+            display.lcd_display_string(
+            programmer[i:i+16],2)
+            wait(0.2)
+        wait(led_speed)
+        display.lcd_clear()
+        
+        threading.Thread(
+        target=RGB_loop).start()
+        
+        for i in range(msbs[0],lsbs[0],-1):
+            display.lcd_display_string(
+            f'{msbs[0]-i:b}',1)
+            display.lcd_display_string(
+            f'H: {msbs[0]-i:X} D: {msbs[0]-i:d}',2)
+            bin=f'{i:b}'
+            for j in range(16):
+                GPIO.output(latch,0)
+                GPIO.output(data_bit,int(bin[j])-1)
+                GPIO.output(clock,1)
+                GPIO.output(latch,1)
+                GPIO.output(clock,0)
+            wait(led_speed)
+            
+        for i in range(lsbs[1],msbs[1]):
+            display.lcd_display_string(
+            f'{i:b}',1)
+            display.lcd_display_string(
+            f'H: {i:X} D: {i:d}',2)
+            bin=f'{i:b}'            
+            for j in range(16):                
+                GPIO.output(latch,0)
+                GPIO.output(data_bit,int(bin[j]))
+                GPIO.output(clock,1)
+                GPIO.output(latch,1)
+                GPIO.output(clock,0)
+            wait(led_speed)
+            
+        for i in range(7):
+            for j in range(16):
+                GPIO.output(latch,0)
+                GPIO.output(data_bit,on_off[i])
+                GPIO.output(clock,1)
+                GPIO.output(latch,1)
+                GPIO.output(clock,0)
+            wait(led_speed)
+        break
+
+# Note: it is recomended that you setup
+# a KeyboardInterrupt handler to force
+# the GPIO pins to return to a low state/off.
+
+# GPIO.cleanup() sets all GPIO pins to LOW/OFF
+
+    except KeyboardInterrupt:
+        exec(stop_program_message)
+        
+        for i in range(16):            
+            GPIO.output(latch,0)
+            GPIO.output(data_bit,0)
+            GPIO.output(clock,1)
+            GPIO.output(latch,1)
+            GPIO.output(clock,0)
+            
+        display.lcd_clear()
+        display.lcd_backlight(0)
+        GPIO.cleanup()
+        break
