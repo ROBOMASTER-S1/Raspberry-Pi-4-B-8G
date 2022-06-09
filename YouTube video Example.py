044375e8ff6e5523b2c53d08f96885f7efb8e631
Joseph C. Richardson <josephcrichardson08@gmail.com>
2022-06-08 22:20:38 -0400

    Update YouTube video Example.py

--- YouTube video Example.py
+++ YouTube video Example.py
@@ -2,7 +2,7 @@
 # example:
 
 # Note: be mindful while working with
-# electroics. There are mistakes that
+# electronics. There are mistakes that
 # cannot be corrected should you ignore
 # any basic electronics rules. Electronics
 # demands basic math skills and knowledge
